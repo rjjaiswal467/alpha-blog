84819c422d90ffaf4ad43bf1fe50bc7d993ba2db
rails udemy course <railsudemycourse@gmail.com>
2016-02-20 14:29:51 +0000

    create user table and models

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121105420) do
+ActiveRecord::Schema.define(version: 20160220141800) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.date     "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
     t.datetime "updated_at"
   end
 
