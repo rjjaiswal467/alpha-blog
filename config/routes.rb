41becc870b1b85903e4092cc563f87ee38f341d1
rails udemy course <railsudemycourse@gmail.com>
2016-02-21 20:57:26 +0000

    add secure passwords

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
