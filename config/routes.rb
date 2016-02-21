1477abd0da5f477a44d581a5ae032ef8bbb388bf
rails udemy course <railsudemycourse@gmail.com>
2016-02-21 21:33:10 +0000

    Signup from ui

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   
   
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  
   
 
   # Example of regular route:
